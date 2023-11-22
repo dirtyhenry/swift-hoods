96d0dd3dd685b104d95375fd647ed025adf6e6be
Mick F <mick@mickf.net>
2023-11-23 00:28:06 +0100

    docs: ✏️ Add documentation
    ChatGPT helped

--- Sources/Hoods/CMS/FrontMatterCMark.swift
+++ Sources/Hoods/CMS/FrontMatterCMark.swift
@@ -2,11 +2,30 @@ import Blocks
 import Foundation
 import Yams
 
-public struct FrontMatterCMark<T: Codable> {
-    public let frontMatter: T?
+/// Represents a combination of front matter (metadata) and CommonMark text.
+///
+/// To learn more about front matters, please read the post I wrote about it on my technical blog:
+/// [*My Definitive Front Matter*][1].
+///
+/// CommonMark is defined as: “a strongly defined, highly compatible specification of Markdown”
+///
+/// You can read more about it on [commonmark.org][2].
+///
+/// [1]: https://bootstragram.com/blog/my-definitive-front-matter/
+/// [2]: https://commonmark.org/
+public struct FrontMatterCMark<FrontMatter: Codable> {
+    /// A front matter of a generic codable type.
+    public let frontMatter: FrontMatter?
+
+    /// The CommonMark text associated with the front matter.
     public let cmark: String
 
-    public init(frontMatter: T?, cmark: String) {
+    /// Initializes a new `FrontMatterCMark` instance.
+    ///
+    /// - Parameters:
+    ///   - frontMatter: The decoded front matter of type `FrontMatter`.
+    ///   - cmark: The CommonMark text associated with the front matter.
+    public init(frontMatter: FrontMatter?, cmark: String) {
         self.frontMatter = frontMatter
         self.cmark = cmark
     }
@@ -16,10 +35,22 @@ enum FrontMatterCMarkUtils {
     public static let frontMatterDelimiter = "---"
 }
 
+/// A decoder for extracting front matter and CommonMark text from a given data.
 public class FrontMatterCMarkDecoder {
+    /// Initializes a new `FrontMatterCMarkDecoder` instance.
     public init() {}
 
-    public func decode<T>(_: T.Type, from data: Data) throws -> FrontMatterCMark<T> where T: Decodable {
+    /// Decodes front matter and CommonMark text from the provided data.
+    ///
+    /// - Parameters:
+    ///   - type: The type to decode the front matter into.
+    ///   - data: The data containing front matter and CommonMark text.
+    /// - Returns: A `FrontMatterCMark` instance with the decoded front matter and CommonMark text.
+    /// - Throws: A decoding error if the front matter cannot be decoded.
+    public func decode<FrontMatter>(
+        _: FrontMatter.Type,
+        from data: Data
+    ) throws -> FrontMatterCMark<FrontMatter> where FrontMatter: Decodable {
         guard let dataAsString = String(data: data, encoding: .utf8) else {
             throw DecodingError.dataCorrupted(.init(codingPath: [], debugDescription: "Could not convert data to UTF8 string."))
         }
@@ -36,7 +67,7 @@ public class FrontMatterCMarkDecoder {
             let cmark = lines[(frontMatterIndex + 1)...].joined(separator: "\n")
 
             let decoder = YAMLDecoder()
-            let frontMatter = try decoder.decode(T.self, from: frontMatterString)
+            let frontMatter = try decoder.decode(FrontMatter.self, from: frontMatterString)
 
             return FrontMatterCMark(
                 frontMatter: frontMatter,
@@ -59,10 +90,19 @@ public class FrontMatterCMarkDecoder {
     }
 }
 
+/// A encoder for encoding `FrontMatterCMark` instances.
 public class FrontMatterCMarkEncoder {
+    /// Initializes a new `FrontMatterCMarkEncoder` instance.
     public init() {}
 
-    public func encode(_ value: FrontMatterCMark<some Any>) throws -> Data {
+    /// Encodes the given `FrontMatterCMark` instance into data.
+    ///
+    /// - Parameter value: The `FrontMatterCMark` instance to encode.
+    /// - Returns: The encoded data.
+    /// - Throws: An encoding error if the front matter cannot be encoded.
+    public func encode(
+        _ value: FrontMatterCMark<some Encodable>
+    ) throws -> Data {
         let frontMatterString = try encodeFrontMatter(value)
         let fullString = [
             frontMatterString,
@@ -74,7 +114,9 @@ public class FrontMatterCMarkEncoder {
         return Data(fullString.utf8)
     }
 
-    private func encodeFrontMatter(_ value: FrontMatterCMark<some Any>) throws -> String {
+    private func encodeFrontMatter(
+        _ value: FrontMatterCMark<some Encodable>
+    ) throws -> String {
         guard let frontMatter = value.frontMatter else {
             return ""
         }
@@ -92,7 +134,9 @@ public class FrontMatterCMarkEncoder {
 
 public extension String {
     func trimmingTrailingCharacters(in _: CharacterSet) -> String {
-        guard let lastIndex = (lastIndex { !CharacterSet(charactersIn: String($0)).isSubset(of: .whitespacesAndNewlines) }) else {
+        guard let lastIndex = (lastIndex { !CharacterSet(charactersIn: String($0))
+                .isSubset(of: .whitespacesAndNewlines)
+        }) else {
             return String(self)
         }
 
