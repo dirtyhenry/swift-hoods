d9e95ba9536d8569a291c2486fbb564e4e9112c3
Mick F <mick@mickf.net>
2024-12-19 15:57:16 +0100

    feat(tca): add counter feature from TCA tutorial

---
+++ Examples/HoodsApp/HoodsApp/TCATutorial/Counter.swift
@@ -0,0 +1,68 @@
+import ComposableArchitecture
+import SwiftUI
+
+@Reducer
+struct CounterFeature {
+    @ObservableState
+    struct State {
+        var count = 0
+    }
+
+    enum Action {
+        case decrementButtonTapped
+        case incrementButtonTapped
+    }
+
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .decrementButtonTapped:
+                state.count -= 1
+                return .none
+
+            case .incrementButtonTapped:
+                state.count += 1
+                return .none
+            }
+        }
+    }
+}
+
+struct CounterView: View {
+    let store: StoreOf<CounterFeature>
+
+    var body: some View {
+        VStack {
+            Text("\(store.count)")
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+            HStack {
+                Button("-") {
+                    store.send(.decrementButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+
+                Button("+") {
+                    store.send(.incrementButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+            }
+        }
+    }
+}
+
+#Preview {
+    CounterView(
+        store: Store(initialState: CounterFeature.State()) {
+            CounterFeature()
+        }
+    )
+}
