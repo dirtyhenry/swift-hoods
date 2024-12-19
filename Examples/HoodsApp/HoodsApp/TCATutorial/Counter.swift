a6a69a4e33dafd54a4e45427587576b6815602fd
Mick F <mick@mickf.net>
2024-12-19 17:40:59 +0100

    feat(tca): add effects

--- Examples/HoodsApp/HoodsApp/TCATutorial/Counter.swift
+++ Examples/HoodsApp/HoodsApp/TCATutorial/Counter.swift
@@ -6,11 +6,22 @@ struct CounterFeature {
     @ObservableState
     struct State {
         var count = 0
+        var fact: String?
+        var isLoading = false
+        var isTimerRunning = false
     }
 
     enum Action {
         case decrementButtonTapped
         case incrementButtonTapped
+        case factButtonTapped
+        case factResponse(String)
+        case timerTick
+        case toggleTimerButtonTapped
+    }
+
+    enum CancelID {
+        case timer
     }
 
     var body: some ReducerOf<Self> {
@@ -18,11 +29,47 @@ struct CounterFeature {
             switch action {
             case .decrementButtonTapped:
                 state.count -= 1
+                state.fact = nil
+                return .none
+
+            case .factButtonTapped:
+                state.fact = nil
+                state.isLoading = true
+                return .run { [count = state.count] send in
+                    let (data, _) = try await URLSession.shared
+                        .data(from: URL(string: "https://www.statium.app/newsletter/api/latest?query=\(count)")!)
+                    let fact = String(decoding: data, as: UTF8.self)
+                    await send(.factResponse(String(fact.prefix(10))))
+                }
+
+            case let .factResponse(fact):
+                state.fact = fact
+                state.isLoading = false
                 return .none
 
             case .incrementButtonTapped:
                 state.count += 1
+                state.fact = nil
                 return .none
+
+            case .timerTick:
+                state.count += 1
+                state.fact = nil
+                return .none
+
+            case .toggleTimerButtonTapped:
+                state.isTimerRunning.toggle()
+                if state.isTimerRunning {
+                    return .run { send in
+                        while true {
+                            try await Task.sleep(for: .seconds(1))
+                            await send(.timerTick)
+                        }
+                    }
+                    .cancellable(id: CancelID.timer)
+                } else {
+                    return .cancel(id: CancelID.timer)
+                }
             }
         }
     }
@@ -54,6 +101,31 @@ struct CounterView: View {
                 .padding()
                 .background(Color.black.opacity(0.1))
                 .cornerRadius(10)
+            }
+
+            Button(store.isTimerRunning ? "Stop timer" : "Start timer") {
+                store.send(.toggleTimerButtonTapped)
+            }
+            .font(.largeTitle)
+            .padding()
+            .background(Color.black.opacity(0.1))
+            .cornerRadius(10)
+
+            Button("Fact") {
+                store.send(.factButtonTapped)
+            }
+            .font(.largeTitle)
+            .padding()
+            .background(Color.black.opacity(0.1))
+            .cornerRadius(10)
+
+            if store.isLoading {
+                ProgressView()
+            } else if let fact = store.fact {
+                Text(fact)
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .padding()
             }
         }
     }
